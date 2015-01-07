dbe8668f73c44fbc3b9516f89b74ca60f7acfd46
Francois Mottard <francoismottard30@gmail.com>
2015-01-06 18:55:33 -0800

    Add Test for Cancel Order

--- TradeWatchTest.cs
+++ TradeWatchTest.cs
@@ -15,27 +15,36 @@ namespace Arbitrage
 
         private static Database db;
 
+        private ExchangeBase exchangeBase;
+
         [ClassInitialize]
         public static void init()
         {
-            Mock.SetupStatic(typeof(WebSocketServer));
-            WebSocketServer server = Mock.Create<WebSocketServer>(Behavior.Strict);
-            Mock.Arrange(() => WebSocketServer.Current).Returns(server);
-            Mock.Arrange(() => server.Broadcast(Arg.IsAny<TradeWatch>(), Arg.IsAny<Subscription>())).DoNothing();
+
+            Mock.SetupStatic(typeof(TradeWatch));
+            MockWebSocketServer();
 
             Mock.SetupStatic(typeof(Database));
+
+            MockTradeLog();
+
+
         }
+
+        
+
+        
 
         [TestInitialize]
         public void setup()
         {
-            Mock.SetupStatic(typeof(TradeWatch));
+            
             watch = Mock.Create<TradeWatch>(Behavior.CallOriginal);
             watch.Id = new Guid();
-            
-            db = Mock.Create<Database>(Behavior.Strict);
+            Mock.Arrange(() => TradeWatch.SaveToDisk()).DoNothing();
 
-            
+            db = Mock.Create<Database>(Behavior.Strict);
+            Mock.Arrange(() => Database.Current).Returns(db);
 
             ExchangeRate exchangeRate = Mock.Create<ExchangeRate>();
             Dictionary<Currency, decimal> rates = new Dictionary<Currency, decimal>();
@@ -44,22 +53,11 @@ namespace Arbitrage
             exchangeRate.Rates = rates;
             db.ExchangeRate = exchangeRate;
 
-            ExchangeBase exchangeBase = Mock.Create<ExchangeBase>(Behavior.Strict);
-            Mock.Arrange(() => exchangeBase.ExecuteTradeOrder(Arg.IsAny<TradeOrder>())).DoInstead((TradeOrder order) => order.Id="ID");
-            Mock.Arrange(() => exchangeBase.PreventPlacingMakerOrderAtMarketPrice).Returns(true);
+            exchangeBase = Mock.Create<ExchangeBase>(Behavior.Strict);
             Mock.Arrange(() => exchangeRate.GetRate(Arg.IsAny<Currency>())).Returns(new Decimal(1));
             
             Mock.Arrange(() => db.GetExchange(Arg.IsAny<Exchange>())).Returns(exchangeBase);
-            Mock.Arrange(() => Database.Current).Returns(db);
-
-            Mock.SetupStatic(typeof(TradeLog));
-            Mock.Arrange(() => TradeLog.Log(Arg.AnyString)).DoNothing();
-
-            Mock.Arrange(() => TradeWatch.SaveToDisk()).DoNothing();
-
-            Mock.SetupStatic(typeof(Debug));
-            Mock.Arrange(() => Debug.Assert(Arg.Matches<bool>(x => x == false))).Throws(new Exception("Debug Assertion"));
-
+           
         }
 
         
@@ -67,7 +65,9 @@ namespace Arbitrage
         [TestMethod]
         public void TestCreateWithoutVolume()
         {
-            
+            Mock.Arrange(() => exchangeBase.ExecuteTradeOrder(Arg.IsAny<TradeOrder>())).DoInstead((TradeOrder o) => o.Id="ID");
+            Mock.Arrange(() => exchangeBase.PreventPlacingMakerOrderAtMarketPrice).Returns(true);
+
             TradeOrder order = new TradeOrder();
             
             watch.Order = order;
@@ -78,6 +78,9 @@ namespace Arbitrage
         [TestMethod]
         public void TestCreateWithoutPrice()
         {           
+            Mock.Arrange(() => exchangeBase.ExecuteTradeOrder(Arg.IsAny<TradeOrder>())).DoInstead((TradeOrder o) => o.Id="ID");
+            Mock.Arrange(() => exchangeBase.PreventPlacingMakerOrderAtMarketPrice).Returns(true);
+
             TradeOrder order = new TradeOrder();
             order.Volume = 100;
             watch.Order = order;
@@ -88,6 +91,8 @@ namespace Arbitrage
         [TestMethod]
         public void TestCreateOrderPriceGreaterThanLastTradePrice()
         {
+            Mock.Arrange(() => exchangeBase.ExecuteTradeOrder(Arg.IsAny<TradeOrder>())).DoInstead((TradeOrder o) => o.Id="ID");
+            Mock.Arrange(() => exchangeBase.PreventPlacingMakerOrderAtMarketPrice).Returns(true);
             MockLastTrade(12.1);
             TradeOrder order = new TradeOrder();
             order.Volume = 100;
@@ -101,9 +106,12 @@ namespace Arbitrage
         [TestMethod]
         public void TestCreateOrderPriceSmallerThanLastTradePrice()
         {
-             
+            
             watch.PairMaker = new CurrencyPair(Exchange.Bitfinex, Currency.BTC, Currency.USD);
             watch.PairTaker = new CurrencyPair(Exchange.BTCChina, Currency.BTC, Currency.CNY);
+
+            Mock.Arrange(() => exchangeBase.ExecuteTradeOrder(Arg.IsAny<TradeOrder>())).DoInstead((TradeOrder o) => o.Id="ID");
+            Mock.Arrange(() => exchangeBase.PreventPlacingMakerOrderAtMarketPrice).Returns(true);
 
             MockLastTrade(12.1);
             TradeOrder order = new TradeOrder();
@@ -118,12 +126,61 @@ namespace Arbitrage
                                         
         }
 
+        [TestMethod]
+        public void TestCancelActiveOrder()
+        {
+            Mock.Arrange(() => exchangeBase.TradeOrderCancel(Arg.IsAny<TradeOrder>(), Arg.AnyString)).DoInstead((TradeOrder o)=>o.IsCancelling = false);
+           
+            TradeOrder order = Mock.Create<TradeOrder>(Behavior.CallOriginal);
+            order.Volume = 100;
+            order.Price = 10;
+            order.Pair = new CurrencyPair(Exchange.Bitfinex, Currency.BTC, Currency.USD);
+            order.Active = true;
+            
+            watch.Order = order;
+            TradeWatch.Trades.Add(watch);
+            watch.Cancel("reason");
+            Assert.IsNull(order.Error);
+        }
+
+        [TestMethod]
+        public void TestCancelExecutedOrder()
+        {
+            Mock.Arrange(() => exchangeBase.TradeOrderCancel(Arg.IsAny<TradeOrder>(), Arg.AnyString)).DoInstead((TradeOrder o)=>o.IsCancelling = false);
+            
+            TradeOrder order = Mock.Create<TradeOrder>(Behavior.CallOriginal);
+            Mock.Arrange(() => order.Status).Returns(TradeOrderStatus.Executed);
+            order.Volume = 100;
+            order.Price = 10;
+            order.Pair = new CurrencyPair(Exchange.Bitfinex, Currency.BTC, Currency.USD);
+            order.Active = true;
+            
+            watch.Order = order;
+            TradeWatch.Trades.Add(watch);
+            watch.Cancel("reason");
+            Assert.IsNull(order.Error);
+        }
+
+
         private void MockLastTrade(double price)
         {
             Trade trade = new Trade();
             trade.Price = new decimal(price);
             Mock.Arrange(() => db.LastPriceGet(Arg.IsAny<CurrencyPair>())).Returns(trade);
         }
-        
+
+        private static void MockWebSocketServer()
+        {
+            Mock.SetupStatic(typeof(WebSocketServer));
+            WebSocketServer server = Mock.Create<WebSocketServer>(Behavior.Strict);
+            Mock.Arrange(() => WebSocketServer.Current).Returns(server);
+            Mock.Arrange(() => server.Broadcast(Arg.IsAny<TradeWatch>(), Arg.IsAny<Subscription>())).DoNothing();
+        }
+     
+        private static void MockTradeLog()
+        {
+            Mock.SetupStatic(typeof(TradeLog));
+            Mock.Arrange(() => TradeLog.Log(Arg.AnyString)).DoNothing();
+        }
     }
 }
